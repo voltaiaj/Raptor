3afb03b8864fb00b654883d9fb3d43962add7e79
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-04-15 17:00:53 -0500

    Bower - Typings - Wallaby

---
+++ Raptor.Web/typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular/angular.d.ts" />
+/// <reference path="angular/angular-mocks.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
