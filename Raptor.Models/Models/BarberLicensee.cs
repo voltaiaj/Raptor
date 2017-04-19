6214cd98686ea865b4acfdb7e664c642daa83d34
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-04-18 20:12:14 -0500

    Adding RaptorContext and BarberLicensee models

---
+++ Raptor.Models/Models/BarberLicensee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Raptor.Models.Models
+{
+    public class BarberLicensee
+    {
+        public int Id { get; set; }
+        public int LicenseNumber { get; set; }
+        public DateTime ExirationDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public string County { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ Raptor.Models/RaptorContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using Raptor.Models.Models;
+
+namespace Raptor.Models
+{
+    public interface IRaptorContext
+    {
+        IDbSet<BarberLicensee> BarberLicensees { get; set; }
+    }
+
+    public class RaptorContext : DbContext, IRaptorContext
+    {
+        internal const string AppSchemaName = "Raptor";
+
+        public RaptorContext()
+            : this("name=Raptor")
+        {
+        }
+
+        private RaptorContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public IDbSet<BarberLicensee> BarberLicensees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<RaptorContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
