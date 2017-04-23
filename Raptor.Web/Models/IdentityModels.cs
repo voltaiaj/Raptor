e808d1e59cfbb4ffdc62f72e6ac1b20cf7f9575c
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-04-22 20:44:04 -0500

    changing connection string

--- Raptor.Web/Models/IdentityModels.cs
+++ Raptor.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Raptor.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("Raptor", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
