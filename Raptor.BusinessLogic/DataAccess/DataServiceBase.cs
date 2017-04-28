fb578f2634b9f1b616756618063020ad5f7ff1fa
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-04-27 19:43:27 -0500

    Adding DataServiceBase and DbSetRepo

---
+++ Raptor.BusinessLogic/DataAccess/DataServiceBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Raptor.BusinessLogic.DataAccess
+{
+    public abstract class DataServiceBase<TEntity>
+        where TEntity : class
+    {
+        protected DataServiceBase()
+        {
+            this.Repository = new DbSetRepository<TEntity>();
+        }
+
+        protected DbSetRepository<TEntity> Repository { get; private set; }
+
+        protected IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            return this.Repository.Find(expression, includes);
+        }
+
+        protected int Count(Expression<Func<TEntity, bool>> expression)
+        {
+            return this.Repository.Count(expression);
+        }
+
+        protected TEntity FirstOfDefault(Expression<Func<TEntity, bool>> expression)
+        {
+            return this.Repository.FirstOrDefault(expression);
+        }
+
+        protected TEntity SingleOrDefault(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            return this.Repository.SingleOrDefault(expression, includes);
+        }
+
+        protected TEntity Add(TEntity entity)
+        {
+            return this.Repository.Add(entity);
+        }
+    }
+}
---
+++ Raptor.BusinessLogic/DataAccess/DbSetRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Raptor.BusinessLogic.DataAccess
+{
+    public class DbSetRepository<TEntity>
+        where TEntity : class
+    {
+        public DbSetRepository()
+        {
+        }
+
+        protected IDbSet<TEntity> DbSet { get; set; }
+        public IQueryable<TEntity> All { get { return this.DbSet.AsQueryable(); } }
+
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            if ((includes == null) || (includes.Length == 0))
+            {
+                return this.All.Where(expression);
+            }
+
+            return includes.Aggregate(this.All.Where(expression), (current, include) => current.Include(include));
+        }
+
+        public TEntity FirstOrDefault(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            if ((includes == null) || (includes.Length == 0))
+            {
+                return this.All.FirstOrDefault(expression);
+            }
+
+            return includes.Aggregate(this.All.Where(expression), (current, include) => current.Include(include)).FirstOrDefault();
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> expression, params Expression<Func<TEntity, object>>[] includes)
+        {
+            if ((includes == null) || (includes.Length == 0))
+            {
+                return this.All.SingleOrDefault(expression);
+            }
+
+            return includes.Aggregate(this.All.Where(expression), (current, include) => current.Include(include)).FirstOrDefault();
+        }
+
+        public TEntity Add(TEntity item)
+        {
+            return this.DbSet.Add(item);
+        }
+
+        public TEntity Remove(TEntity item)
+        {
+            return this.DbSet.Remove(item);
+        }
+
+        public int Count(Expression<Func<TEntity, bool>>  expression)
+        {
+            return this.DbSet.Count(expression);
+        }
+    }
+}
