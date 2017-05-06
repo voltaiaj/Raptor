017f537120db6347840b39ca6dd5b97a0b53b2b8
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-05-05 20:18:42 -0500

    filling out dataServiceHelper

--- Raptor.Web/app/utils/dataServiceHelper.service.ts
+++ Raptor.Web/app/utils/dataServiceHelper.service.ts
@@ -3,6 +3,12 @@
 
     export interface IDataServiceHelper {
         get(url: string): angular.IHttpPromise<any>;
+        getWithKey(url: string, key: number): angular.IHttpPromise<any>;
+        getWithStringKey(url: string, key: string): angular.IHttpPromise<any>;
+        getWithQuerystring(url: string, params: any): angular.IHttpPromise<any>;
+        postWithParameters(url: string, params: any): angular.IHttpPromise<any>;
+        postWithParametersForFileUpload(url: string, params: any): angular.IHttpPromise<any>;
+        postWithoutParameters(url: string): any;
     }
 
     export class DataServiceHelper implements IDataServiceHelper {
@@ -17,7 +23,36 @@
             return this.$http.get(fullUrl);
         }
 
+        public getWithKey(url: string, key: number) {
+            let fullUrl: string = this.urlHelper.addTrailingSlashOnUrl(url) + key;
+            return this.$http.get(fullUrl);
+        }
 
+        public getWithStringKey(url: string, key: string) {
+            let fullUrl: string = this.urlHelper.addTrailingSlashOnUrl(url) + key;
+            return this.$http.get(fullUrl);
+        }
+
+        public getWithQuerystring<T>(url: string, params: T) {
+            return this.$http.get(this.urlHelper.removeTrailingSlashOnUrl(url), { params: params });
+        }
+
+        public postWithParameters<T>(url: string, params: T) {
+            return this.$http.post(this.urlHelper.removeTrailingSlashOnUrl(url), params);
+        }
+
+        public postWithoutParameters(url: string) {
+            return this.$http.post(this.urlHelper.removeTrailingSlashOnUrl(url), {});
+        }
+
+        public postWithParametersForFileUpload<T>(url: string, params: T) {
+            return this.$http.post(this.urlHelper.removeTrailingSlashOnUrl(url),
+                params,
+                {
+                    transformRequest: angular.identity,
+                    headers: { 'Content-Type': undefined }
+                });
+        }
     }
 
     angular.module('utils')
