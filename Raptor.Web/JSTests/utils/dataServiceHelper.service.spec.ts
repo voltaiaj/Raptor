31076eec55e09bece2cb32d30096ce7038a9c172
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-05-09 20:36:54 -0500

    adding tests

---
+++ Raptor.Web/JSTests/utils/dataServiceHelper.service.spec.ts
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+describe('Utils dataServiceHelper', () => {
+    let sut: Utils.IDataServiceHelper,
+        $httpBackend: angular.IHttpBackendService,
+        actual;
+
+    beforeEach(() => {
+        angular.mock.module('utils');
+
+        inject((_dataServiceHelper_: Utils.IDataServiceHelper, _$httpBackend_: angular.IHttpBackendService) => {
+            sut = _dataServiceHelper_;
+            $httpBackend = _$httpBackend_;
+        });
+    });
+
+    describe('get', () => {
+        
+    });
+});
