3d330037c95ba09ee374710134a4cbe160673d34
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-05-10 20:18:49 -0500

    adding tests for get

--- Raptor.Web/JSTests/utils/dataServiceHelper.service.spec.ts
+++ Raptor.Web/JSTests/utils/dataServiceHelper.service.spec.ts
@@ -15,6 +15,60 @@ describe('Utils dataServiceHelper', () => {
     });
 
     describe('get', () => {
-        
+        let fulfilledCallbackSpy,
+            rejectedCallbackSpy,
+            response,
+            expectedUrl = 'myUrl',
+            expectedSuccessResponse = { someProperty: 'jane' },
+            expectedErrorResponse = 'There was an error...';
+
+        beforeEach(() => {
+            fulfilledCallbackSpy = sinon.spy();
+            rejectedCallbackSpy = sinon.spy();
+            actual = sut.get('myUrl/')
+                .then(fulfilledCallbackSpy, rejectedCallbackSpy);
+        });
+
+        afterEach(() => {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should be defined on the service and GET from the expected URL', () => {
+            expect(sut.get).toBeDefined();
+            $httpBackend.expectGET(expectedUrl).respond(true);
+            $httpBackend.flush();
+        });
+
+        it('should execute the onFulFulled callback of the promise when the request is successful', () => {
+            $httpBackend.expectGET(expectedUrl).respond(200, expectedSuccessResponse);
+            $httpBackend.flush();
+
+            expect(fulfilledCallbackSpy.calledOnce).toBe(true);
+            expect(rejectedCallbackSpy.called).toBe(false);
+
+            response = fulfilledCallbackSpy.getCall(0).args[0];
+            expect(response.status).toBeDefined();
+            expect(response.status).toBe(200);
+            expect(response.data).toBeDefined();
+            expect(response.data).toEqual(expectedSuccessResponse);
+            expect(response.data).not.toEqual(expectedErrorResponse);
+        });
+
+        it('should execute the error callback of the promise when the request when the request is NOT successful', () => {
+            $httpBackend.expectGET(expectedUrl).respond(400, expectedErrorResponse);
+            $httpBackend.flush();
+
+            expect(rejectedCallbackSpy.calledOnce).toBe(true);
+            expect(fulfilledCallbackSpy.called).toBe(false);
+
+            response = rejectedCallbackSpy.getCall(0).args[0];
+            expect(response.status).toBeDefined();
+            expect(response.status).toBe(400);
+            expect(response.data).toBeDefined();
+            expect(response.data).toEqual(expectedErrorResponse);
+            expect(response.data).not.toEqual(expectedSuccessResponse);
+
+        });
     });
 });
