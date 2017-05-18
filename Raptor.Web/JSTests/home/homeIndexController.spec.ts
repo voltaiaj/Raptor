36ea6f75ff921214006bf8263f47f3f5605925bb
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-05-17 19:34:15 -0500

    adding homeIndexController test

---
+++ Raptor.Web/JSTests/home/homeIndexController.spec.ts
@@ -0,0 +1,31 @@
+﻿namespace Raptor.Home.Test {
+    'use strict';
+
+    describe('homeIndexController', () => {
+        let sut: IHomeIndexController,
+            $state: angular.ui.IStateService,
+            $scope: angular.IScope,
+            $controller: angular.IControllerService;
+
+        beforeEach(() => {
+            angular.mock.module('ui.router');
+            angular.mock.module('utils');
+            angular.mock.module('raptor.home');
+
+            inject((_$controller_: angular.IControllerService,
+                _$state_: angular.ui.IStateService,
+                _$rootScope_: angular.IRootScopeService) => {
+                $controller = _$controller_;
+                $state = _$state_;
+                $scope = _$rootScope_.$new();
+            });
+
+            sut = <IHomeIndexController>$controller('homeIndexController');
+            $scope.$digest();
+        });
+
+        it('should be defined', () => {
+            expect(sut).toBeDefined();
+        });
+    });
+}
