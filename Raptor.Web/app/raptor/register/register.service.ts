da27cb7cbdaf70cf38f0f9629d0fefe9d886452d
Voltaiaj <Alexander.Voltaire@gmail.com>
2017-05-23 20:45:23 -0500

    adding register service

---
+++ Raptor.Web/app/raptor/register/register.service.ts
@@ -0,0 +1,15 @@
+﻿namespace Raptor.Register {
+    'use strict';
+
+    export interface IRegisterService {
+    }
+
+    export class RegisterService implements IRegisterService {
+        public static $inject: string[] = [''];
+
+        constructor() { }
+    }
+
+    angular.module('raptor.register')
+        .service('registerService', RegisterService);
+}
