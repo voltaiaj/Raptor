20b7e9d05593028feba2daa31a12939e78a3dffb
Alexander Voltaire <Alexander.Voltaire@nov.com>
2017-07-20 20:15:10 -0500

    adding D3 bower and typings

---
+++ Raptor.Web/app/d3/d3.module.ts
@@ -0,0 +1,14 @@
+﻿namespace D3 {
+    //'use strict';
+
+    angular.module('d3', [])
+        .factory('d3Service', [
+            function() {
+                var d3;
+
+                d3 = window.;
+
+                return d3;
+            }
+        ]);
+}
